93b6da68d5ed50a8f38daae70dc7a8c05cbef69b
Pavan Kumar Vaitheeswaran <pavaninkumar@gmail.com>
2022-11-19 20:04:10 -0800

    Add cubic function to Integral python moule

--- Python/Integral.py
+++ Python/Integral.py
@@ -4,6 +4,13 @@ given function
 """
 def f(x):
 	return x**2
+
+def g(x):
+    return x**3
+
 from scipy.integrate import quad
-quad(f,0.,2.)
-print quad
+squareFunc = quad(f,0.,2.)
+cubeFunc = quad(g,0.,2.)
+
+print squareFunc
+print cubeFunc
